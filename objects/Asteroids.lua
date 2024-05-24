1354c5ef5a3540d2e621e98a11b63a18e9f50122
yodaShogun <pierrechriswesley@gmail.com>
2024-05-24 07:35:44 -0400

    drawing the shapes of the asteroids

--- objects/Asteroids.lua
+++ objects/Asteroids.lua
@@ -29,6 +29,8 @@ function Asteroids(x, y, ast_size, level, debugging)
         xVel = math.random() * AsteroidsSpeed * vel,
         yVel = math.random() * AsteroidsSpeed * vel,
         radius = math.ceil(ast_size/2),
+        vert = vert,
+        offset = offset,
 
         draw = function (self,faded)
             local opcaity = 1
@@ -40,9 +42,20 @@ function Asteroids(x, y, ast_size, level, debugging)
             love.graphics.setColor(186/255, 189/255, 182/255, opcaity)
 
             local points = {
-                self.x + self.radius * self.offset
-
+                self.x + self.radius * self.offset[1] * math.cos(self.angle),
+                self.y + self.radius * self.offset[1] * math.sin(self.angle)
             }
+            for i=1, self.vert - 1 do
+                table.insert(points, self.x + self.radius * self.offset[i+1] * math.cos(self.angle + i * math.pi * 2 / self.vert))
+                table.insert(points, self.y + self.radius * self.offset[i+1] * math.cos(self.angle + i * math.pi * 2 / self.vert))
+            end
+
+            love.graphics.polygon("line", points)
+
+            if debugging then
+                love.graphics.setColor(1 , 0, 0)
+                love.graphics.circle("line", self.x, self.y, self.radius)
+            end
         end
 
 
